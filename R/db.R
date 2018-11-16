bcf823e2bd868f23b7a5e69ec4a9039eae7a3006
Andrea Ghelfi <32156639+kdri-genomics@users.noreply.github.com>
2018-11-16 17:01:02 +0900

    Add files via upload

--- R/db.R
+++ R/db.R
@@ -1,11 +1,19 @@
 db <-
 function() {
   dokohayai <- system.file(package="hayai")
-  dokohayai <- paste(dokohayai,"data", sep="/")
+  dokodata <- paste(dokohayai,"data", sep="/")
+  dokoR <- paste(dokohayai,"R", sep="/")
   dokoftp <- "ftp://ftp.kazusa.or.jp/pub/hayai/hayai-annotation/plants"
-  names <- c("phyta_aa.udb","phyta_ab.udb","phyta_ac.udb","phyta_ad.udb")
-  for (j in 1:4) {
-  destfile <- paste(dokohayai, names[j], sep="/")
+
+  names2 <- c("sysdata.rda") 
+  destfile2 <- paste(dokoR, names2, sep="/")
+  ftp2 <- paste(dokoftp, names2, sep="/")
+  download.file(ftp2, destfile2)
+ 
+  names <- c("phyta_aa.udb","phyta_ab.udb","phyta_ac.udb","phyta_ad.udb","info_database.rda", "mf_table.rda","bp_table.rda","cc_table.rda")
+  l <- length(names)
+  for (j in 1:l) {
+  destfile <- paste(dokodata, names[j], sep="/")
   ftp <- paste(dokoftp, names[j], sep="/")
   download.file(ftp, destfile)
   }
